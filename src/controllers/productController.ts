b8fa828773fec205a4fb1c955aae6477ebaeb259
ivanmatey <ivanmatey5@gmail.com>
2024-11-12 20:27:09 -0700

    added styles to home page

--- src/controllers/productController.ts
+++ src/controllers/productController.ts
@@ -4,7 +4,12 @@ import { getProducts } from "../models/productModel";
 export const getProductsList = async (req: Request, res: Response) => {
   try {
     const products = await getProducts();
-    res.render("pages/products", { products: products });
+    const data = {
+      user: {
+        name: req.session.user?.name
+      }
+    };
+    res.render("pages/products", { products: products, user: data.user });
   } catch (error) {
     console.error(error);
     res.status(500).send("Error fetching product data");
