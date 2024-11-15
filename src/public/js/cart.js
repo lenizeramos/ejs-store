b7182b49bd5f64d4b86dc69dcd0f0c7917aad05f
Lenize Ramos <lenizemr@gmail.com>
2024-11-15 09:30:29 -0800

    wip

--- src/public/js/cart.js
+++ src/public/js/cart.js
@@ -1,24 +1,26 @@
 $("#updateCartForm").on("submit", function (event) {
-    event.preventDefault();
-    var formData = $(this).serializeArray();
-    var data = {};
-    formData.forEach(function (field) {
-        data[field.name] = field.value;
-    });
-    $.ajax({
-        url: "/cart/update",
-        method: "PUT",
-        contentType: "application/json",
-        data: JSON.stringify({
-            productId: data.productId,
-            quantity: data.quantity
-        }),
-        success: function (response) {
-            window.location.href = "/cart";
-        },
-        error: function (xhr, status, error) {
-            console.error("Error:", error);
-            alert("An error occurred while updating the cart.");
-        }
-    });
+  event.preventDefault();
+
+  const formData = $(this).serializeArray();
+  const data: { [key: string]: string } = {};
+  formData.forEach((field) => {
+    data[field.name] = field.value;
+  });
+
+  $.ajax({
+    url: "/cart/update",
+    method: "PUT",
+    contentType: "application/json",
+    data: JSON.stringify({
+      productId: data.productId,
+      quantity: data.quantity
+    }),
+    success: function (response) {
+      window.location.href = "/cart";
+    },
+    error: function (xhr, status, error) {
+      console.error("Error:", error);
+      alert("An error occurred while updating the cart.");
+    }
+  });
 });
