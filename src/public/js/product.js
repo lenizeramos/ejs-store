ad8d5bdbe93a37d29252dfe41fa7d3a3fd3499cd
ivanmatey <ivanmatey5@gmail.com>
2024-11-16 11:59:31 -0700

    adding modals

--- src/public/js/product.js
+++ src/public/js/product.js
@@ -1,28 +1,51 @@
 $(function () {
     console.log("New");
     var url = "localhost:3000/cart/add";
-    var postData = function (id) {
-        $.ajax({
-            url: url,
-            type: 'POST',
-            data: {
-                productId: id
-            },
-            success: function (response) {
-                console.log("Server Response: ", response);
-            },
-            error: function (status, error) {
-                console.log("Error: ", error);
-            }
-        });
-    };
+    // const postData = (id:number)=>{
+    //     $.ajax({
+    //         url: url,
+    //         type: 'POST',
+    //         data:{
+    //             productId:id
+    //         },
+    //         success: function(response){
+    //             console.log("Server Response: ", response)
+    //         },
+    //         error: function(status, error){
+    //             console.log("Error: ", error)
+    //         }
+    //     })
+    // }
     var addToCartButtons = $(".add-to-cart");
     addToCartButtons.on('click', function (e) {
         var target = $(e.currentTarget);
         var productId = target.data('product-id');
-        postData(productId);
+        // postData(productId)
         console.log("Product ID: ", productId);
     });
+    var moreDetails = $(".more-details");
+    moreDetails.on('click', function (e) {
+        var target = $(e.currentTarget);
+        var description = target.data('product-description');
+        var modal = target.closest(".wrapper-container").find(".product-modal");
+        var modalContent = target.closest('wrapper-container').find('modal-content');
+        var descriptionElement = $("\n            <p>Description</p>\n            <p class = \"product-description\">".concat(description, "</p>\n            "));
+        modal.css('display', 'block');
+        modalContent.append(descriptionElement);
+        console.log("Description: ", description);
+    });
+    var closeTag = $(".close-tag");
+    closeTag.on('click', function (e) {
+        var target = $(e.currentTarget);
+        var modal = target.closest(".wrapper-container").find(".product-modal");
+        modal.css('display', 'none');
+    });
+    // $(window).on('click', (event)=>{
+    //     const modal = $(this).closest(".wrapper-container").find(".product-modal")
+    //     if($(event.target).is(modal)){
+    //         modal.css('display','none')
+    //     }
+    // })
 });
 // $("#1").on("click", ()=>{
 //     const item = $(this).closest(".item-wrapper1").find(".product-title1").text();
--- src/public/ts/product.ts
+++ src/public/ts/product.ts
@@ -3,21 +3,21 @@ $(()=>{
 
     const url = "localhost:3000/cart/add";
 
-    const postData = (id:number)=>{
-        $.ajax({
-            url: url,
-            type: 'POST',
-            data:{
-                productId:id
-            },
-            success: function(response){
-                console.log("Server Response: ", response)
-            },
-            error: function(status, error){
-                console.log("Error: ", error)
-            }
-        })
-    }
+    // const postData = (id:number)=>{
+    //     $.ajax({
+    //         url: url,
+    //         type: 'POST',
+    //         data:{
+    //             productId:id
+    //         },
+    //         success: function(response){
+    //             console.log("Server Response: ", response)
+    //         },
+    //         error: function(status, error){
+    //             console.log("Error: ", error)
+    //         }
+    //     })
+    // }
 
     const addToCartButtons = $(".add-to-cart");
     
@@ -25,10 +25,47 @@ $(()=>{
         const target = $(e.currentTarget);
         const productId = target.data('product-id') as number;
 
-        postData(productId)
+        // postData(productId)
 
         console.log("Product ID: ", productId)
     })
+
+    const moreDetails = $(".more-details");
+
+    moreDetails.on('click', (e:JQuery.ClickEvent) => {
+        const target = $(e.currentTarget);
+        const description = target.data('product-description') as string
+        const modal = target.closest(".wrapper-container").find(".product-modal")
+        const modalContent = target.closest('wrapper-container').find('modal-content');
+
+        const descriptionElement = $(`
+            <p>Description</p>
+            <p class = "product-description">${description}</p>
+            `)
+
+            modal.css('display','block')
+            modalContent.append(descriptionElement);
+        
+        
+        console.log("Description: ", description)
+        
+    })
+
+    const closeTag = $(".close-tag")
+    closeTag.on('click', (e:JQuery.ClickEvent)=>{
+        const target = $(e.currentTarget);
+
+        const modal = target.closest(".wrapper-container").find(".product-modal")
+        modal.css('display','none')
+        
+    })
+    // $(window).on('click', (event)=>{
+    //     const modal = $(this).closest(".wrapper-container").find(".product-modal")
+
+    //     if($(event.target).is(modal)){
+    //         modal.css('display','none')
+    //     }
+    // })
 
 });
 
